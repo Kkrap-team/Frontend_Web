982547f7683c39fde96985507179438400cab905
JaeYoonJung08 <whitecy01@naver.com>
2024-08-17 22:18:21 +0900

    feat: main page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1>일상 속 모든 것을<br /><span className="highlight">스크랩, 크크랩</span></h1>
+        <div className="logo">
+            <img src="/logo.png" alt="크크랩 로고" />
+        </div>
+        <button className="kakao-button">카카오톡 계정으로 시작하기</button>
+      </div>
     </div>
   );
 }
