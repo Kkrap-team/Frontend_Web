bb5c10a70a6220a33dea6ddbbeaba464587dc76d
dlghwls06 <dlghwls06@naver.com>
2025-04-12 23:29:29 +0900

    feat : 헤더 기능, 검색바 추가, 유저 정보 화면에 표시, 헤더 전체 css 추가

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from '../components/layout/layout';
 import MainPage from '../pages/MainPage/MainPage';
 import LoginPage from '../pages/LoginPage/LoginPage';
+import Header from '../components/layout/Header';
+// import ProfilePage from ''
 
 function AppRouter() {
     return (
         <Router>
-            <Routes>
-                <Route path="/" element={<MainPage />} />
-                <Route path="/login" element={<LoginPage />} />
-                <Route path="*" element={<div>페이지를 찾을 수 없습니다.</div>} />
-            </Routes>
+            <Header />
+            <Layout>
+                <Routes>
+                    <Route path="/" element={<MainPage />} />
+                    <Route path="/login" element={<LoginPage />} />
+                    <Route path="*" element={<div>페이지를 찾을 수 없습니다.</div>} />
+                    {/* <Route path="/profile" element= {<ProfilePage/> }/> */}
+                </Routes>
+            </Layout>
         </Router>
     );
 }
