e1250c4781d3bce74bd6b788ee899f7897a0a7cf
whitecy01 <whitecy01@naver.com>
2025-08-25 23:40:45 +0900

    LoginPage, UserPage import 해결

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -6,9 +6,9 @@ import Layout from '@/components/layout/Layout';
 import { useAuthStore } from '@/stores/authStore';
 
 import MainPage from '@/pages/MainPage/MainPage';
-import LoginPage from '@/pages/loginPage/LoginPage';
+import LoginPage from '@/pages/LoginPage/LoginPage';
 import FollowerPage from '@/pages/FollowerPage/FollowerPage';
-import EditProfilePage from '@/pages/userPage/EditProfilePage';
+import EditProfilePage from '@/pages/UserPage/EditProfilePage';
 import StoragePage from '@/pages/storagePage/StoragePage';
 import StorageFolderDetail from '@/pages/storageFolderDetail/StorageFolderDetail';
 import SearchPage from '@/pages/SearchPage/SearchPage';
